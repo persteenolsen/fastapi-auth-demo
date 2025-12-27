b62d518f1b9cd64f6d459c6a96e1b044b4de9ea2
Per Olsen <persteen22@gmail.com>
2025-12-27 16:17:24 +0100

    The routes and service files should be ready

--- routes/user.py
+++ routes/user.py
@@ -1,10 +1,6 @@
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
-# Import the database session dependency
-from db.database import get_db
-from sqlalchemy.orm import Session
-
 # Import the get_current_username and get_current_user functions from services/users.py
 from services.users import get_current_username, get_current_user
 from services.users import get_all_users, get_access_token_for_login, do_register_user
@@ -16,29 +12,16 @@ from models.user import User
 # To Avoid confusion / conflict with the names of Models we import the schemas Objects as:
 # UserSchema, UserCreateSchema and TokenSchema
 from schemas.user import User as UserSchema
-from schemas.user import UserCreate as UserCreateSchema
 from schemas.token import Token as TokenSchema
 
 router_auth = APIRouter()
 
-# Note: An old version of the below endpoint is kept commented out for reference
-# @router_auth.post("/register", response_model=UserSchema, tags=["user"])
-'''def register_user(user: UserCreateSchema, db: Session = Depends(get_db)):
-    new_user = do_register_user(user, db)
-    return new_user
-'''
 # Public route that returns access token and type if User credentials are valid
 # 27-12-2025 - The endpoint needs to be /token for using the OpenAPI Autorize button
 # Note: User Registration Endpoint disabled for Production
-@router_auth.post("/register", response_model=UserSchema, tags=["user"])
+#@router_auth.post("/register", response_model=UserSchema, tags=["user"])
 def register_user(new_user = Depends(do_register_user)):
     return new_user
-
-# Note: An old version of the below endpoint is kept commented out for reference
-'''@router_auth.post("/token", response_model=TokenSchema, tags=["user"])
-    def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    return get_access_token_for_login(form_data ,db)
-'''
 
 # Public route that returns access token and type if User credentials are valid
 # 27-12-2025 - The endpoint needs to be /token for using the OpenAPI Autorize button
--- services/users.py
+++ services/users.py
@@ -19,21 +19,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 # Note: The below to functions are placed in service/user.py for auth related functions in 
 # order to separate concerns and make the code more modular
-'''def do_register_user(user, db):
-    db_user = db.query(User).filter(User.username == user.username).first()
-    if db_user:
-        raise HTTPException(status_code=400, detail="Username already registered")
-    
-    hashed_password = get_password_hash(user.password)
-    
-    # 25-12-2025 - Added Users Name
-    new_user = User(username=user.username, name=user.name, email=user.email, hashed_password=hashed_password)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
-'''
+# Public route that registers a new User in the PostgreSQL Database
 def do_register_user(user: UserCreateSchema, db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.username == user.username).first()
     if db_user:
@@ -49,19 +35,6 @@ def do_register_user(user: UserCreateSchema, db: Session = Depends(get_db)):
 
     return new_user
 
-# Public route that returns access token and type if User credentials are valid
-'''def get_access_token_for_login(form_data, db):
-    user = db.query(User).filter(User.username == form_data.username).first()
-    if not user or not verify_password(form_data.password, user.hashed_password):
-        
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token = create_access_token(data={"sub": user.username})
-    return {"access_token": access_token, "token_type": "bearer"}
-'''
 # Public route that returns access token and type if User credentials are valid
 # Note: This function uses Dependency Injection for form_data and db session
 def get_access_token_for_login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
